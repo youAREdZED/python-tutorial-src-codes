f88ea184f4f44b7a3cd8eed1b0fb26aa4c46e48e
youAREdZED <youwedoubleyouxyzed@gmail.com>
2025-02-06 11:45:28 +0330

    edit

--- os/python-file-explorer.py
+++ os/python-file-explorer.py
@@ -1,47 +1,50 @@
 import os
+
 option = 0
+
 while option != 5:
+
     print("1.goto ")
     print("2.open file")
     print("3.delete file")
     print("4.copy file")
-    print("5.exit")
-    option = int(input("enter your option : "))
-
+    print("5.quit")
+    option = int(input("enter option : "))
     if option == 1:
-        path = input("enter path : ")
+        path = input("enter your path : ")
         try:
             os.chdir(path)
             print("now you are on : ", path)
         except:
-            print("something went wrong")
+            print("something went wrong!")
     elif option == 2:
-        file_name = input("name : ")
-        file_path = input("path : ")
-        file_type = input("type : ")
+        file_path = input("enter your file path : ")
+        file_name = input("enter your file name : ")
+        file_type = input("enter your file type : ")
         try:
-            path = r"{x}{y}.{z}".format(x = file_path, y = file_name, z = file_type)
+            path = r"{x}{y}.{z}".format(x=file_path, y=file_name, z=file_type)
             os.system("start " + path)
+            print("file now is open")
         except:
-            print("someting went wrong !")
+            print("something went wrong!")
     elif option == 3:
-        file_name = input("name : ")
-        file_path = input("path : ")
-        file_type = input("type : ")
+        file_path = input("enter your file path : ")
+        file_name = input("enter your file name : ")
+        file_type = input("enter your file type : ")
         try:
-            path = r"{x}{y}.{z}".format(x = file_path, y = file_name, z = file_type)
+            path = r"{x}{y}.{z}".format(x=file_path, y=file_name, z=file_type)
             os.remove(path)
-            print("file deleted !")
+            print("your file removed successfully")
         except:
-            print("something went wrong !")
+            print("something went wrong!")
     elif option == 4:
-        file_name = input("name : ")
-        file_path = input("path : ")
-        file_type = input("type : ")
-        file_dist = input("dist : ")
+        file_path = input("enter your file path : ")
+        file_name = input("enter your file name : ")
+        file_type = input("enter your file type : ")
+        file_dist = input("enter your file dist : ")
+        path = r'{x}{y}.{z}'.format(x=file_path, y=file_name, z=file_type)
         try:
-            src = r"{x}{y}.{z}".format(x = file_path, y = file_name, z = file_type)
-            cmd = f'"copy "{src}" "{file_dist}"'
+            cmd = f'copy "{path}" "{file_dist}"'
             os.system(cmd)
         except:
             print("something went wrong !")
